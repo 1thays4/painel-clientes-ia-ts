5cf409f486b1476a8661fe3a1c6f0c9e3de93e04
1thays4 <thayscosta66@gmail.com>
2026-04-08 11:15:59 -0300

    chore: remove deprecated components and scripts for cleaner codebase

--- src/utils/testarFormatacao.ts
+++
@@ -1,6 +0,0 @@
-import { formatarTelefone } from "./formatarTelefone";
-
-// Exemplos de uso
-console.log(formatarTelefone("554791767425")); // Deve retornar: +55 (47) 9 1767-7425
-console.log(formatarTelefone("+554791767425")); // Deve retornar: +55 (47) 9 1767-7425
-console.log(formatarTelefone("4791767425")); // Deve retornar: (47) 9 1767-7425
